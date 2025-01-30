92b9f0f594c0bd7758b378a62ac66f3fd6ef023e
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-01-30 19:58:09 +0530

    setuping the sign-in and sign-up pages

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -9,15 +9,18 @@ const layout = ({ children }: { children: ReactNode }) => {
             <Image src="/icons/logo.svg" alt="logo" width={37} height={37} />
             <h1 className="text-2xl font-semibold text-white">BookSage</h1>
           </div>
-          <div>
-            {children}
-          </div>
+          <div>{children}</div>
         </div>
       </section>
 
       <section className="auth-illustration">
-
-        <Image src="/images/auth-illustration.png" alt="auth illustration" width={1000} height={1000} className="size-full object-cover"/>
+        <Image
+          src="/images/auth-illustration.png"
+          alt="auth illustration"
+          width={1000}
+          height={1000}
+          className="size-full object-cover"
+        />
       </section>
     </main>
   );
---
+++ components/AuthForm.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// interface Props <T extends FeildValues>
+
+
+const AuthForm = ({type, scheme, defaultValues, onSubmit}: Props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AuthForm
