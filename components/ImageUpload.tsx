b9b280ac811b94a6da284f9c2788003f9be62c7c
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-06-11 23:43:30 +0530

    imagekit

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -1,4 +1,21 @@
+'use client'
 import React from 'react'
+
+
+import { IKImage, ImageKitProvider, IKUpload } from 'imagekitio-next' 
+
+
+const authenticator = async () => {
+  try {
+    const response = await fetch();
+
+
+
+  } catch(error: any){
+    throw new Error(message: `Authentication request failed: ${error.message }`);
+  }
+}
+
 
 const ImageUpload = () => {
   return (
@@ -9,3 +26,6 @@ const ImageUpload = () => {
 }
 
 export default ImageUpload
+
+
+//1:43:50 
--- lib/config.ts
+++ lib/config.ts
@@ -1,5 +1,6 @@
 const config = {
     env: {
+        apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
         imagekit : {
             publickey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY,
             urlEndpoint: process.env.NEXT_PUBLIC_API_URL_ENDPOINT,
