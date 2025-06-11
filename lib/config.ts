ab4c1666b2d64a2fb09001aef0c17b986a7ee3fa
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-06-11 23:00:42 +0530

    imagekit api keys

---
+++ lib/config.ts
@@ -0,0 +1,11 @@
+const config = {
+    env: {
+        imagekit : {
+            publickey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY,
+            urlEndpoint: process.env.NEXT_PUBLIC_API_URL_ENDPOINT,
+            privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+        },
+    },
+};
+
+export default config;
