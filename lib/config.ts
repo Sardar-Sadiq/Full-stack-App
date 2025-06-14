637d03df44f7b4032ef37ea9a3b669771e43359b
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-06-14 18:18:26 +0530

    image kit

--- lib/config.ts
+++ lib/config.ts
@@ -2,9 +2,9 @@ const config = {
     env: {
         apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
         imagekit : {
-            publickey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY,
-            urlEndpoint: process.env.NEXT_PUBLIC_API_URL_ENDPOINT,
-            privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+            publickey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
+            urlEndpoint: process.env.NEXT_PUBLIC_API_URL_ENDPOINT!,
+            privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
         },
     },
 };
